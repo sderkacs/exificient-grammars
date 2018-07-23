cf3dcbcbfb538601e4048c5183a9d344b392bfd6
danielpeintner <daniel@peintner.org>
2018-07-23 12:58:11 +0200

    get ready for next release version 1.0.1

--- src/main/java/com/siemens/ct/exi/grammars/EXIContentModelBuilder.java
+++ src/main/java/com/siemens/ct/exi/grammars/EXIContentModelBuilder.java
@@ -81,7 +81,7 @@ import com.siemens.ct.exi.core.util.sort.StartElementSort;
  * @author Daniel.Peintner.EXT@siemens.com
  * @author Joerg.Heuer@siemens.com
  * 
- * @version 1.0.1-SNAPSHOT
+ * @version 1.0.1
  */
 
 public abstract class EXIContentModelBuilder extends CMBuilder implements
--- src/main/java/com/siemens/ct/exi/grammars/XSDGrammarsBuilder.java
+++ src/main/java/com/siemens/ct/exi/grammars/XSDGrammarsBuilder.java
@@ -133,7 +133,7 @@ import com.siemens.ct.exi.grammars.regex.EXIRegularExpression;
  * @author Daniel.Peintner.EXT@siemens.com
  * @author Joerg.Heuer@siemens.com
  * 
- * @version 1.0.1-SNAPSHOT
+ * @version 1.0.1
  */
 
 public class XSDGrammarsBuilder extends EXIContentModelBuilder {
